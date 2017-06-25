3c31fdbb80526e7c770f3187a266fb49755b04be
Daniel Gullberg <daniel_gullberg@hotmail.com>
2017-06-26 00:31:48 +0200

    Revert "line length updates"
    This reverts commit 9e78470d2bb0626f2acb60463a1b25d75f404bda.
    
    This should make the listing file more readable

--- macro.c
+++ macro.c
@@ -88,7 +88,7 @@ do_endm(int *ip)
 struct t_macro *macro_look(int *ip)
 {
 	struct t_macro *ptr;
-	char name[128];
+	char name[32];
 	char c;
 	int  hash;
 	int  l;
@@ -106,7 +106,7 @@ struct t_macro *macro_look(int *ip)
 			if (isdigit(c))
 				return (NULL);
 		}
-		if (l == 127)
+		if (l == 31)
 			return (NULL);
 		name[l++] = c;
 		hash += c;
